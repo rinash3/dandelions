67793aef21725e71c44dc74ad7f74fcdf1749ccb
hngan <hngan@cs.stonybrook.edu>
2019-08-06 14:41:32 -0400

    added mongodb

--- client/src/components/share/button.js
+++ client/src/components/share/button.js
@@ -3,7 +3,7 @@ import "./UIbasics.css"
 
 function Button(props) {
     return(
-        <button onClick={props.click}><h2>Next<span>-></span></h2></button>
+        <button onClick={props.click}><h2>Next -></h2></button>
     )
 }
 
