bdc4e7762881ba38d013920b4cdfd3e81c7be4f2
hngan <hngan@cs.stonybrook.edu>
2019-08-06 14:44:42 -0400

    added mongodb

--- models/giver.js
+++ models/giver.js
@@ -3,7 +3,6 @@ const Schema = mongoose.Schema;
 
 const GiverSchema=new Schema({
     email:{type:String, required:true},
-    name:{type:String, required:true},
     password:{type:String, required:true},
     pastGifts:{type:Array, required:true, default:[]},
     tickets:{type:Number, required:true, default:0}
