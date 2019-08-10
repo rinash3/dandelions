854d509f954dada6700a6b82fde1d1357699cd63
Isabella Wang <wangxbella0108@gmail.com>
2019-08-10 15:48:38 -0400

    make loading and 404 page

---
+++ client/src/components/share/loading.js
@@ -0,0 +1,17 @@
+import React from "react";
+// import ReactDOM from "react-dom";
+import "./loading.css"
+class Loading extends React.Component {
+
+    render() {
+
+        return (
+           <div class="loading">
+	        <div></div>
+            </div>
+	
+        );
+    }
+}
+
+export default Loading;
