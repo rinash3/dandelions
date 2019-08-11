4394de84a55f6aa67af51b0b68fe58bbefeba887
Rina Razumov <rinash3@gmail.com>
2019-08-11 10:31:22 -0400

    fixed issues

--- client/src/components/giver/home/index.js
+++ client/src/components/giver/home/index.js
@@ -7,69 +7,62 @@ import Giverfill from "../giverfill"
 import API from "../../../utils/API"
 
 class Giverhome extends Component {
-    state={
-        email:"",
-        password:"",
-        registered:false,
-        account:{}
+    state = {
+        email: "",
+        password: "",
+        registered: false,
+        account: {}
     }
     handleInputChange = event => {
         const { name, value } = event.target;
         this.setState({
-          [name]: value
+            [name]: value
         });
-      };
-    click = event =>{
+    };
+    click = event => {
         event.preventDefault();
-        API.getGiver(this.state.email).then(res=>{
-            if(res.data){
-                this.setState({account:res.data, registered:true})
+        API.getGiver(this.state.email).then(res => {
+            if (res.data) {
+                this.setState({ account: res.data, registered: true })
                 console.log(res.data);
-                if(res.data.password === this.state.password)
+                if (res.data.password === this.state.password)
                     console.log("You were able to log in!");
                 else
                     alert("Incorrect password!")
             }
-            else{
-                let person={
-                    email:this.state.email,
-                    password:this.state.password
+            else {
+                let person = {
+                    email: this.state.email,
+                    password: this.state.password
                 }
                 API.createGiver(person).then(console.log("Created new person"))
             }
         })
     }
     render() {
-<<<<<<< HEAD
-  
-        const someMargin={
-            marginBottom:'2em'
-        }
 
-=======
+
+
         //if account found and password is correct redirect to main page for specified giver
-        if(this.state.registered)
+        if (this.state.registered)
             return <Giverfill person={this.state.account} />
         //else just keep it the page we are on now
->>>>>>> 0e62b9ccd9451833e5988b2b3c98e3d7654d90cb
         return (
-            
+
             <div>
-                <Giverhomepagebg/>
+                <Giverhomepagebg />
                 <form className="hp-form">
                     <h1 className="white hp-h1">Fullfill a wish</h1>
-                    <a className="white size-28" href="/wisher">Make a wish</a>    
+                    <a className="white size-28" href="/wisher">Make a wish</a>
                     <label className="white beth" htmlFor="email">Email:</label>
                     <input onChange={this.handleInputChange} id="giverEmail" name="email" className="white" type="email"></input>
-                    <br/>
+                    <br />
                     <label className="white beth" htmlFor="password">Password:</label>
-                    <input  onChange={this.handleInputChange} id="giverPassword" name="password" className="white" type="password"></input>
-                    <br/><br/>
-<<<<<<< HEAD
-                    <Button className="btn" click={this.click} location="/giver/fills"/>
-=======
-                    <Button className="btn" style={this.props.position} click={this.click} location="/giver/fills"/>
->>>>>>> 0e62b9ccd9451833e5988b2b3c98e3d7654d90cb
+                    <input onChange={this.handleInputChange} id="giverPassword" name="password" className="white" type="password"></input>
+                    <br /><br />
+
+                    <Button className="btn" style={this.props.position} click={this.click} location="/giver/fills" />
+
                 </form>
             </div>
         );
--- client/src/components/giver/printAddress/button.js
+++ client/src/components/giver/printAddress/button.js
@@ -5,13 +5,8 @@ import { Link } from "react-router-dom";
 function Button() {
 
     return (
-<<<<<<< HEAD
-        <Link to="/giver/fills" className="z-index beth giver-print-left">Fullfill another wish -></Link>
-=======
 
         <Link to="/giver/fills" className="z-index beth">Fullfill another wish -></Link>
-
->>>>>>> 0e62b9ccd9451833e5988b2b3c98e3d7654d90cb
     );
 }
 
