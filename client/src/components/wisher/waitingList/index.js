addad1a6f1254b2ccdf2bbe80af243fea8ab4809
Isabella Wang <wangxbella0108@gmail.com>
2019-08-12 15:16:41 -0400

    update

--- client/src/components/wisher/waitingList/index.js
+++ client/src/components/wisher/waitingList/index.js
@@ -5,6 +5,20 @@ import bgvideo from "./paper.mp4"
 import ProfilePopup from "./profile"
 
 class WaitingList extends Component {
+    state = {
+        email: "test@test.com",
+        password: "123",
+        firstName: "test",
+        lastName: "test",
+        address: "test",
+        apt: "44",
+        city: "test",
+        zipCode: "2345"
+    };
+
+    submit = (e) => {
+        console.log("hi");
+    }
     closeProfile = (e) => {
         e.preventDefault();
         document.getElementById("ProfilePopup").style.display = "none"
@@ -18,12 +32,18 @@ class WaitingList extends Component {
     render() {
         return (
             <div>
-                 <a className="maroon size-28" href="..." onClick={this.openProfile}>Profile</a>
+                <a className="maroon size-28" href="..." onClick={this.openProfile}>Profile</a>
                 <video className="paper" autoPlay loop muted>
                     <source src={bgvideo} type='video/mp4' />
                 </video>
                 <h1 className="paper-text">Your wish is heard, we will contact you soon</h1>
-                <ProfilePopup id="ProfilePopup" Close={this.closeProfile} content={this.state.list}>
+                <ProfilePopup submit={this.submit} id="ProfilePopup" Close={this.closeProfile}
+                    email={this.state.email} password={this.state.password} firstName={this.state.firstName}
+                    lastName={this.state.lastName}
+                    address={this.state.address}
+                    apt={this.state.apt}
+                    city={this.state.city}
+                    zipCode={this.state.zipCode}>
                 </ProfilePopup>
             </div>
         );
